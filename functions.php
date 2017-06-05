2eaae2fc7a060089b1f23ebaaad1ec859710d9b5
Sprincis <sprincis.janis@oxior.eu>
2017-06-05 14:09:11 +0300

    v1.6.9.2 - Checked WordPress 4.8 compatibility

--- functions.php
+++ functions.php
@@ -428,7 +428,7 @@ function beryl_register_required_plugins() {
 			'name'     				=> __('Bootstrap 3 Shortcodes','beryl'), // The plugin name
 			'slug'     				=> 'bootstrap-3-shortcodes', // The plugin slug (typically the folder name)
 			'required' 				=> false, // If false, the plugin is only 'recommended' instead of required
-			'version' 				=> '3.3.6', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
+			'version' 				=> '3.3.10', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
 			'force_activation' 		=> false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
 			'force_deactivation' 	=> false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
 			'external_url' 			=> '', // If set, overrides default API URL and points to an external URL
